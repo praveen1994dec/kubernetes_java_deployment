531d1a7fce7c592849f66356a4cf9af2cd034a81
kamaleshchalla <89450355+kamaleshchalla@users.noreply.github.com>
2023-12-01 09:54:12 +0530

    Update healthcheck.java

--- stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
+++ stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
@@ -1,13 +1,14 @@
-// package com.example.springbootmanagementexample;
+ package uk.co.danielbryant.djshopping.stockmanager;
+
  
-// import java.util.Date;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RestController;
+ import java.util.Date;
+ import org.springframework.web.bind.annotation.GetMapping;
+ import org.springframework.web.bind.annotation.RestController;
  
-// @RestController
-// public class SimpleRestController {
-//     @GetMapping("/example")
-//     public String example() {
-//         return "Hello User !! " + new Date();
-//     }
-// }
+ @RestController
+ public class healthcheck {
+     @GetMapping("/actuator/health")
+     public String healthcheck() {
+         return "Hello User !! " + new Date();
+     }
+ }
