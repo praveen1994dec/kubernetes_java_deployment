e03bf0c348f52805b9d2be554816f5c1ff8337fd
awskutty <132982524+awskutty@users.noreply.github.com>
2023-12-23 19:31:27 +0530

    Update StockManagerApplication.java

--- stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/StockManagerApplication.java
+++ stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/StockManagerApplication.java
@@ -1,5 +1,15 @@
 package uk.co.danielbryant.djshopping.stockmanager;
+import java.util.Date;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class healthcheck {
+@GetMapping("/actuator/health")
+public String healthcheck() {
+return "Hello User !! " + new Date();
+}
+}
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
